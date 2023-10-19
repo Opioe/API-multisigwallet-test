931d79b3dd47d9708ef3cbdec0dc1b92fc2d04ff
Clement Rodallec <crodallec@juniorisep.com>
2023-10-19 10:10:51 +0200

    fix sever4.js now deploy MultiSigWallet

--- server4.js
+++ server4.js
@@ -2,7 +2,7 @@ const ethers = require("ethers");
 const dotenv = require('dotenv');
 const hre = require('hardhat');
 dotenv.config();
-const { INFURA_API_KEY, WALLET_PRIVATE_KEY, QUICKNODE_API_KEY } = process.env;
+const { WALLET_PRIVATE_KEY, QUICKNODE_API_KEY } = process.env;
 
 const RPCurl = 'https://attentive-convincing-pallet.matic-testnet.quiknode.pro/' + QUICKNODE_API_KEY + '/';
 
@@ -13,13 +13,13 @@ async function main() {
     const provider = new ethers.JsonRpcProvider(RPCurl);
     const wallet = new ethers.Wallet(WALLET_PRIVATE_KEY, provider);
 
+    const nonce = await wallet.getNonce();
+
     const contractArtifact = await hre.artifacts.readArtifact('MultiSigWallet');
     const contractBytecode = contractArtifact.bytecode;
     const contractABI = contractArtifact.abi;
     const arg1 = ["0x07128963Cafd50de0E338b3234ECe7C6D4F100D5", "0x31236F42F4d3c052cc4720Da7CFFc74C9A1dA2B0"];
     const arg2 = 1;
-    console.log(contractBytecode);
-    console.log(contractABI);
 
     const contractFactory = new ethers.ContractFactory(contractABI, contractBytecode, wallet);
 
@@ -28,11 +28,11 @@ async function main() {
     const data = contractBytecode + encodeConstructorArgs.slice(2);
     console.log("deploying with transaction data");
     const tx = {
-        nonce: 7,
+        nonce: nonce,
         to: null,
         value: 0,
-        gasPrice: 79520000000000,
-        gasLimit: 3150,
+        gasPrice: 1860267955,
+        gasLimit: 21000000,
         data: data,
         chainId: 80001,
     };
